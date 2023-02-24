2394804ad008648fb6d08e321b57fcb3cf289efc
zanedr <zanedanielrussell@gmail.com>
2023-02-23 18:38:10 -0700

    remove unused code

--- src/signWithLit.ts
+++ src/signWithLit.ts
@@ -46,30 +46,13 @@ async function signCosmosTxWithLit({pkpPublicKey, message, authSig}: SignCosmosT
     return;
   }
 
-  // console.log('message', message)
-  // const hashedWithEthers = hashMessageUsingEthers(message);
-  // console.log('hashedWithEthers', hashedWithEthers);
-
-  // const uint8ArrayFromMessage = Uint8Array.from(message);
-  // const hashedWithCosm = sha256(ethers.utils.toUtf8Bytes(message));
-  // console.log('hashedWithCosm', hashedWithCosm);
-  //
-  // const stubMessage = 'stub message'
-  // const hashedWithCosmStub = sha256(ethers.utils.toUtf8Bytes(stubMessage));
-  // const buffer = Buffer.from(hashedWithCosmStub.slice(2), 'hex');
-  // const hashedStubArray = new Uint8Array(buffer);
-  // console.log('hashedWithCosmStub', hashedWithCosmStub);
-  // const pkpPublicKeyStub = "0x0460579cb0d4bb4846ba43a001e7ec6c42e4db62f3b9c40358df31392e90b24952217c60c945c434dfb68bfc1e5b7cd89b41d1180bdd12b76a9f1d2f82b6d0d6db"
-
   const hashedWithCosm = sha256(ethers.utils.toUtf8Bytes(message));
-  console.log('hashedWithCosm', hashedWithCosm);
   const buffer = Buffer.from(hashedWithCosm.slice(2), 'hex');
   const hashedArray = new Uint8Array(buffer);
 
   const jsParams = {
     publicKey: pkpPublicKey,
     toSign: hashedArray,
-    // toSign: hashedWithCosm,
     sigName: 'cosmos',
   }
 
@@ -85,8 +68,6 @@ async function signCosmosTxWithLit({pkpPublicKey, message, authSig}: SignCosmosT
     console.log('Unable to execute code', err);
     return;
   }
-
-  // const arrayifiedMessage = ethers.utils.arrayify(message);
 
   console.log('litActionRes', ethers.utils.recoverPublicKey(hashedWithCosm, litActionRes.signatures.cosmos.signature));
 
